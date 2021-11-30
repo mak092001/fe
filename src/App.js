dca2eec9d25d9712d110c249ac0b816be14059f4
mak092001 <private.mak09@gmail.com>
2021-11-30 23:21:35 +0530

    op

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
 function App() {
+  let [data, setdata] = useState();
+  fetch("https://git.heroku.com/gentle-hamlet-63750.git")
+    .then((op) => op.json())
+    .then((op) => setdata(op))
+    .catch((err) => console.log(err));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mak- {data}</h1>
     </div>
   );
 }
